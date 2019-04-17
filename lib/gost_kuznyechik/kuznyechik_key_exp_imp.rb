f8c726cb829c4c68decb247633a20ef5e84d0f71
vblazhnov <vblazhnov@yandex.ru>
2019-04-17 11:57:15 +0300

    Key export/import test

--- lib/gost_kuznyechik/kuznyechik_key_exp_imp.rb
+++ lib/gost_kuznyechik/kuznyechik_key_exp_imp.rb
@@ -1,17 +1,19 @@
 module GostKuznyechik
   class KuznyechikKeyExpImp < Kuznyechik
     def self.export(key, key_mac, key_enc, iv)
-      mac = KuznyechicOmac.new(key_mac, BlockLengthInBytes).update(iv).update(key).final
+      mac = KuznyechikOmac.new(key_mac, BlockLengthInBytes).update(iv+key).final
       ctr = KuznyechikCtr.new(key_enc, iv, BlockLengthInBytes)
-      encr_key = ctr.encrypt(key) + ctr.encrypt(mac)
+      encr_key = ctr.encrypt(key)
+      encr_mac = ctr.encrypt(mac)
+      encr_key += encr_mac
     end
     
     def self.import(encr_key, key_mac, key_enc, iv)
       buf = KuznyechikCtr.new(key_enc, iv, BlockLengthInBytes).decrypt(encr_key)
       decr_key = buf[0...-BlockLengthInBytes]
       decr_mac = buf[decr_key.length..-1]
-      mac = KuznyechicOmac.new(key_mac, BlockLengthInBytes).update(iv).update(decr_key).final
-      if mac ~= decr_mac then
+      mac = KuznyechikOmac.new(key_mac, BlockLengthInBytes).update(iv+decr_key).final
+      if mac != decr_mac then
         decr_key = nil
       end
       decr_key
